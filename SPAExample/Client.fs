098a59c621cc22fe3dc720a858c03a6f26f8383a
Andre Dublin <81dublin@gmail.com>
2016-09-17 19:06:15 -0400

    init

--- SPAExample/Client.fs
+++ SPAExample/Client.fs
@@ -1,36 +1,79 @@
 namespace SPAExample
 
+open System
 open WebSharper
 open WebSharper.JavaScript
+open WebSharper.UI.Next.Html
 open WebSharper.JQuery
 open WebSharper.UI.Next
 open WebSharper.UI.Next.Client
+open FSharp.Data
+open WebSharper.Charting
 
 [<JavaScript>]
-module Client =    
-    type IndexTemplate = Templating.Template<"index.html">
+module Client = 
+    type WorldBank = WorldBankDataProvider< Asynchronous=true >
+    
+    let data = WorldBank.GetDataContext()
+    
+    let randomColor = 
+        let r = Random()
+        fun () -> Color.Rgba(r.Next 256, r.Next 256, r.Next 256, 1.)
+    
+    let countries = 
+        [| data.Countries.Austria
+           data.Countries.Hungary
+           data.Countries.``United Kingdom``
+           data.Countries.``United States`` |]
 
-    let People =
-        ListModel.FromSeq [
-            "John"
-            "Paul"
+    let colors = Array.map (fun _ -> randomColor()) countries
+    
+    let mkData (i : Runtime.WorldBank.Indicator) =
+        Seq.zip (Seq.map string i.Years) i.Values
+        
+    let chart = 
+        let cfg =
+            ChartJs.LineChartConfiguration(
+                PointDot = false,
+                BezierCurve = true,
+                DatasetFill = false)
+                
+        async {
+            let! data =
+                countries
+                |> Seq.map (fun c -> c.Indicators.``School enrollment, tertiary (gross), gender parity index (GPI)``)
+                |> Async.Parallel
+            return
+                data
+                |> Array.map mkData
+                |> Array.zip colors
+                |> Array.map (fun (c, e) ->
+                    Chart.Line(e)
+                        .WithStrokeColor(c)
+                        .WithPointColor(c))
+                |> Chart.Combine
+                |> fun c ->
+                    Renderers.ChartJs.Render(c, Size(600, 400), Config = cfg)
+        }
+
+    let legend =
+        colors
+        |> Array.zip countries
+        |> Array.map (fun (c, color) ->
+            div [
+                spanAttr ["width:200px;height:20px;\
+                          margin-right:10px;\
+                          display:inline-block;\
+                          background-color:" + color.ToString() |> attr.style] []
+                span [text c.Name]
+            ])
+        |> Seq.cast
+        |> div
+    
+    let Main = 
+        Doc.Concat [
+            h2 [text "Teritary school enrollment (% gross)"]
+            Doc.Async chart
+            legend
         ]
-
-    let Main =
-        JQuery.Of("#main").Empty().Ignore
-
-        let newName = Var.Create ""
-
-        IndexTemplate.Main.Doc(
-            ListContainer = [
-                People.View.DocSeqCached(fun name ->
-                    IndexTemplate.ListItem.Doc(Name = View.Const name)
-                )
-            ],
-            Name = newName,
-            Add = (fun el ev ->
-                People.Add(newName.Value)
-                newName.Value <- ""
-            )
-        )
         |> Doc.RunById "main"
